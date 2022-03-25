f430b328a7d560bcc94c524daa78afcec245452e
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2022-03-25 12:47:51 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './assets/libs/boxicons-2.1.1/css/boxicons.min.css'
-import './scss/App.scss'
-import Blank from './pages/Blank'
+import "./assets/libs/boxicons-2.1.1/css/boxicons.min.css";
+import "./scss/App.scss";
+import Blank from "./pages/Blank";
 
 function App() {
-    return (
-        <div>
-            <Blank />
-        </div>
-    )
+  return (
+    <div>
+      <Blank />
+      <h1>Metronic</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
